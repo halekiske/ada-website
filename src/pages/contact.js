18cb6d0d6378602dd06883b2b4f4532773bf4549
Halekiske <halekiske@gmail.com>
2025-06-04 16:53:17 +0800

    Update Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,6 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -5,6 +8,29 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 function AppContact() {
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'your_service_id',        // 🔁 replace with your EmailJS service ID
+      'your_template_id',       // 🔁 replace with your EmailJS template ID
+      formRef.current,
+      'your_public_key'         // 🔁 replace with your EmailJS public key
+    ).then(
+      (result) => {
+        alert("Message sent successfully!");
+        console.log(result.text);
+        e.target.reset();
+      },
+      (error) => {
+        alert("Message failed to send.");
+        console.error(error.text);
+      }
+    );
+  };
+
   return (
     <section id="contact" className="block contact-block">
       <Container fluid>
@@ -12,21 +38,21 @@ function AppContact() {
           <h2>Contact us</h2>
           <div className="subtitle">get connected with us</div>
         </div>
-        <Form className='contact-form'>
+        <Form className='contact-form' ref={formRef} onSubmit={sendEmail}>
           <Row>
             <Col sm={4}>
-              <Form.Control type="text" placeholder="Enter your full name" required />
+              <Form.Control name="name" type="text" placeholder="Enter your full name" required />
             </Col>
             <Col sm={4}>
-              <Form.Control type="email" placeholder="Enter your email address" required />
+              <Form.Control name="email" type="email" placeholder="Enter your email address" required />
             </Col>
             <Col sm={4}>
-              <Form.Control type="tel" placeholder="Enter your contact number" required />
+              <Form.Control name="contact" type="tel" placeholder="Enter your contact number" required />
             </Col>
           </Row>
           <Row>
             <Col sm={12}>
-              <Form.Control as="textarea" placeholder="Enter your contact message" required />
+              <Form.Control name="message" as="textarea" placeholder="Enter your contact message" required />
             </Col>
           </Row>
           <div className='btn-holder'>
@@ -35,7 +61,16 @@ function AppContact() {
         </Form>
       </Container>
       <div className='google-map'>
-        <iframe title="map" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=ada surveying services&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="400" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
+        <iframe
+          title="map"
+          src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=ada surveying services&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          width="100%"
+          height="400"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+        ></iframe>
       </div>
       <Container fluid>
         <div className='contact-info'>
